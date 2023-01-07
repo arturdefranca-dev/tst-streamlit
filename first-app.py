ec6c6411e5c53a538d9d8537b600cc75eff89c10
arturdefranca-dev <121910773+arturdefranca-dev@users.noreply.github.com>
2023-01-07 11:02:21 -0300

    Update first-app
    Testando o submit

--- first-app.py
+++ first-app.py
@@ -5,7 +5,11 @@ import streamlit as st
 with st.form(key="include_cliente"):
   input_name = st.text_input(label="Insira o seu nome")
   input_age = st.number_input(label="Insira sua idade", format="%d", step=1)
-  input_occupatio = st.selectbox(label="Selecione sua profissão", options=["Desenvolvedor","Músico","Designer","Professor"])
+  input_occupation = st.selectbox(label="Selecione sua profissão", options=["Desenvolvedor","Músico","Designer","Professor"])
   input_button_submit = st.form_submit_button("Enviar")
                                  
-
+with input_button_submit:
+  st.write(f'Nome: {input_name}')
+  st.write(f'Idade: {input_age}')
+  st.write(f'Profissão: {input_occupation}')
+  
