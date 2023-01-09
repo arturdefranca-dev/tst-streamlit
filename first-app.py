51a8c3e230489f48f8678de58048dbf1c6d2bb97
arturdefranca-dev <121910773+arturdefranca-dev@users.noreply.github.com>
2023-01-09 07:15:41 -0300

    Update first-app.py
    Criando DB

--- first-app.py
+++ first-app.py
@@ -1,11 +1,26 @@
 # streamlit_app.py
 
 import streamlit as st
+import mysql.connector
 
-with st.form(key="include_cliente"):
-  input_name = st.text_input(label="Insira o seu nome")
-  input_age = st.number_input(label="Insira sua idade", format="%d", step=1)
-  input_occupatio = st.selectbox(label="Selecione sua profissão", options=["Desenvolvedor","Músico","Designer","Professor"])
-  input_button_submit = st.form_submit_button("Enviar")
-                                 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
 
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
